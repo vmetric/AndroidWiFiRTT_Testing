e51b2b10aae4e438a846299e5424e8f43ead5104
Matt <armada2526pbem@gmail.com>
2020-11-08 14:12:18 -0700

    Check for WiFi RTT
    Add check for if WiFi RTT exists on device hardware. Prints message accordingly (albeit, a message with improper grammar)
    
    Thus I've discovered my S10+ does not have RTT. Shoot.

--- app/src/main/java/net/staela/wifirtt_testing/MainActivity.kt
+++ app/src/main/java/net/staela/wifirtt_testing/MainActivity.kt
@@ -1,11 +1,22 @@
 package net.staela.wifirtt_testing
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.text.TextUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainTextView = findViewById<TextView>(R.id.mainTextView)
+
+        if (this.packageManager.hasSystemFeature(PackageManager.FEATURE_WIFI_RTT)) {
+            mainTextView.setText("Has WiFi RTT!")
+        } else {
+            mainTextView.setText("Not has WiFi RTT! :(")
+        }
     }
 }
